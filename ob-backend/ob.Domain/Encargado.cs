85461fdea51b748c15ce826178ee962e0ca01515
Nacho Etcheverry <ignacio.etcheverry@ical.com.uy>
2024-04-08 20:34:36 -0300

    Encargado

--- ob-backend/ob.Domain/Encargado.cs
+++ ob-backend/ob.Domain/Encargado.cs
@@ -3,5 +3,24 @@ namespace ob.Domain
     public class Encargado : Usuario
     {
         public List<Edificio> Edificios { get; set; }
+
+        public Encargado(string nombre, string apellido, string email, string contrasena)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+            Contrasena = contrasena;
+            Edificios = new List<Edificio>();
+        }
+
+        //public Solicitud CrearSolicitud(string descripcion, Depto departamento, Categoria categoria)
+        //{
+            //return new Solicitud(descripcion, departamento, categoria);
+        //}
+
+        //public void AsignarSolicitud(Solicitud solicitud, Mantenimiento personal)
+        //{
+            // Lógica para asignar la solicitud
+        //}
     }
 }

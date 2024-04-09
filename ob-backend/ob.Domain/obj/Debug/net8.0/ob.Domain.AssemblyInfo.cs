25598cbe7311b2189a9105daab4f076b6ba42cfb
Nacho Etcheverry <ignacio.etcheverry@ical.com.uy>
2024-04-09 12:31:59 -0300

    TDD Persona MAntenimiento

--- ob-backend/ob.Domain/obj/Debug/net8.0/ob.Domain.AssemblyInfo.cs
+++ ob-backend/ob.Domain/obj/Debug/net8.0/ob.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ob.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab596f09bb2f8848051b628ef9ffddb740707a7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1f51a486cacb163a4a5a7ad371303b4d63e55c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ob.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ob.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ob-tests/ob.Domain.Tests/MantenimientoTest.cs
+++ ob-tests/ob.Domain.Tests/MantenimientoTest.cs
@@ -4,7 +4,20 @@ namespace ob.Domain.Tests;
 public class MantenimientoTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public void NuevoMantenimiento()
     {
+        //Arrange & Act
+        Mantenimiento nuevoMantenimiento = new Mantenimiento(
+            "Juan",
+            "Sosa",
+            "jsosa@gmail.com",
+            "Contra12345"
+        );
+
+        //Assert
+        Assert.AreEqual("Juan", nuevoMantenimiento.Nombre);
+        Assert.AreEqual("Sosa", nuevoMantenimiento.Apellido);
+        Assert.AreEqual("jsosa@gmail.com", nuevoMantenimiento.Email);
+        Assert.AreEqual("Contra12345", nuevoMantenimiento.Contrasena);
     }
 }

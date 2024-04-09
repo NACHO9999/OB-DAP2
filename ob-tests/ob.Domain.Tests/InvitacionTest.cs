ea1f51a486cacb163a4a5a7ad371303b4d63e55c
Nacho Etcheverry <ignacio.etcheverry@ical.com.uy>
2024-04-09 12:28:21 -0300

    TDD Invitacion

--- ob-tests/ob.Domain.Tests/InvitacionTest.cs
+++ ob-tests/ob.Domain.Tests/InvitacionTest.cs
@@ -4,7 +4,25 @@ namespace ob.Domain.Tests;
 public class InvitacionTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public void NuevaInvitacion()
     {
+        //Arrange
+
+        string email = "jsosa@gmail.com";
+        string nombre = "Juan";
+        DateTime fechaExpiracion = DateTime.Now;
+
+        //Act
+        Invitacion nuevaInvitacion = new Invitacion
+        {
+            Email = email,
+            Nombre = nombre,
+            FechaExpiracion = fechaExpiracion
+        };
+
+        //Assert
+        Assert.AreEqual(email, nuevaInvitacion.Email);
+        Assert.AreEqual(nombre, nuevaInvitacion.Nombre);
+        Assert.AreEqual(fechaExpiracion, nuevaInvitacion.FechaExpiracion);
     }
 }

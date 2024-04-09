5c5d0b7aafa86f16d9b312a7d926b283dedafd04
Nacho Etcheverry <ignacio.etcheverry@ical.com.uy>
2024-04-09 12:04:20 -0300

    Gitignore Redo

--- ob-tests/ob.Domain.Tests/obj/Debug/net8.0/ob.Domain.Tests.AssemblyInfo.cs
+++ ob-tests/ob.Domain.Tests/obj/Debug/net8.0/ob.Domain.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ob.Domain.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9070f44f9e6b60ae2ccbc8f88d9fb11a5295b5fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5256ba4d57e7dfae0ef025007486266cdd912349")]
 [assembly: System.Reflection.AssemblyProductAttribute("ob.Domain.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ob.Domain.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

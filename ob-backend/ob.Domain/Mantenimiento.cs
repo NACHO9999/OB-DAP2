6a50fd99ec28d18b3439afc57f875622e06238d0
Nacho Etcheverry <ignacio.etcheverry@ical.com.uy>
2024-04-11 18:06:04 -0300

    Mantenimiento sol

--- ob-backend/ob.Domain/Mantenimiento.cs
+++ ob-backend/ob.Domain/Mantenimiento.cs
@@ -1,15 +1,14 @@
 namespace ob.Domain
 {
-    public class Mantenimiento: Usuario
+    public class Mantenimiento : Usuario
     {
         public Mantenimiento(string nombre, string apellido, string email, string contrasena)
+            : base(nombre, apellido, email, contrasena)
         {
             Nombre = nombre;
             Apellido = apellido;
             Email = email;
             Contrasena = contrasena;
         }
-
-        public Mantenimiento() { }
     }
 }

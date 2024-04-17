388bc2add05ed854700d28c5697ddec32c313652
Nacho Etcheverry <nachoetcheverry@gmail.com>
2024-04-17 10:03:40 -0300

    Agregado BusinessLogic e IBusinessLogic

---
+++ ob-backend/ob.BusinessLogic/AdminService.cs
@@ -0,0 +1,10 @@
+using ob.Domain;
+using ob.IBusinessLogic;
+
+namespace ob.BusinessLogic;
+
+public class AdminService : IAdminService
+{
+    public void CrearAdmin(Administrador admin){}
+    public void BorrarAdmin(int id){}
+}
---
+++ ob-backend/ob.IBusinessLogic/IAdminService.cs
@@ -0,0 +1,9 @@
+﻿using ob.Domain;
+
+namespace ob.IBusinessLogic;
+
+public interface IAdminService
+{
+    void CrearAdmin(Administrador admin);
+    void BorrarAdmin(int id);
+}

65ed918ddfd341158b0da25a8d83f46f297df26c
Nacho Etcheverry <nachoetcheverry@gmail.com>
2024-04-30 11:37:22 -0300

    arreglar tests

--- ob-backend/ob.Domain/obj/Debug/net8.0/ob.Domain.AssemblyInfo.cs
+++ ob-backend/ob.Domain/obj/Debug/net8.0/ob.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ob.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b926f93e68800ac4e9f2281f7bca4d5a8f1bf508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558bf28c5aa2276ee2dbd9a9b580b1ed58e268a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ob.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ob.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

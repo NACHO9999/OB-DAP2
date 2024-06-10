5775aeb1caf4cde0fa66a828dfa807867157da95
Nacho Etcheverry <nachoetcheverry@gmail.com>
2024-06-10 08:50:22 -0300

    Esto

--- ob-backend/ob.Domain/obj/Debug/net8.0/ob.Domain.AssemblyInfo.cs
+++ ob-backend/ob.Domain/obj/Debug/net8.0/ob.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ob.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c654ee1e893d8e29a43f7f2bc15e1ade09c35d70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351d06738831cf8bbb9001b16dcc2b08bdfc7e3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ob.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ob.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
